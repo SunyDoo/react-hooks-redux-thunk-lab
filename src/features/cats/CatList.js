1b23f5130f8c9aeeab370e0e94a7d37e4843c61c
Sunil <snilavarath@gmail.com>
2022-05-24 17:27:01 -0400

    commit tree 1e15cef325a39dcd666cdfa1a431709f238710e5

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,12 @@
 // write your CatList component here
+import React from "react";
+
+export default function CatList({ catPics }) {
+  return (
+    <>
+      {catPics.map((cat) => (
+        <img key={cat.id} src={cat.url} alt="cat" />
+      ))}
+    </>
+  );
+}
--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,37 @@
-export function fetchCats() {}
+export function fetchCats() {
+  return function (dispatch) {
+    // dispatch an initial action to set up a "loading" state
+    dispatch({
+      type: "cats/catsLoading",
+    });
 
-export default function catsReducer() {}
+    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: "cats/catsLoaded", payload: data.images });
+      });
+  };
+}
+
+const initialState = {
+  entities: [], // array of cats
+  status: "idle", // loading state
+};
+
+export default function catsReducer(state = initialState, action) {
+  switch (action.type) {
+    case "cats/catsLoading":
+      return {
+        ...state,
+        status: "loading",
+      };
+    case "cats/catsLoaded":
+      return {
+        ...state,
+        entities: action.payload,
+        status: "idle",
+      };
+    default:
+      return state;
+  }
+}
